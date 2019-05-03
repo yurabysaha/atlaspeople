83ecda76d79e40ccc334d21a27029fd45a42d1f2
yurabysaha <yurabysaha@gmail.com>
2019-05-03 16:07:42 +0300

    Add sitemap for google

--- atlaspeople/urls.py
+++ atlaspeople/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.contrib.sitemaps.views import sitemap
 
+from atlaspeople.sitemaps import StaticViewSitemap
 
+sitemaps = {
+    'static': StaticViewSitemap,
+}
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('website.urls')),
+
+    path('sitemap.xml', sitemap, {'sitemaps': sitemaps},
+        name='django.contrib.sitemaps.views.sitemap')
 ]
